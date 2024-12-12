951a4986cb24ec072dfd91ef25101639150b1bf6
Max Moiseev <max@maxmoiseev.com>
2024-12-11 22:02:51 -0800

    Day 11.2

--- apps/aoc2024/src/day11.erl
+++ apps/aoc2024/src/day11.erl
@@ -1,7 +1,6 @@
 -module(day11).
 
 -export([part_1/0, part_2/0]).
--export([step/2, steps/2]).
 
 -define(DAY, 11).
 
@@ -10,33 +9,46 @@ part_1() ->
     %Data = <<"125 17\n">>,
     [Line] = aoclib:lines(Data),
     Ns = aoclib:numbers(Line),
-    length(steps(Ns, 25)).
+    solve(Ns, 25).
 
 part_2() ->
-    ok.
+    {ok, Data} = aoclib:get_data(?DAY),
+    %Data = <<"125 17\n">>,
+    [Line] = aoclib:lines(Data),
+    Ns = aoclib:numbers(Line),
+    solve(Ns, 75).
 
-steps(Xs, 0) ->
-    Xs;
-steps(Xs, N) ->
-    steps(step(Xs, []), N - 1).
+blink(N) ->
+    case N of
+        0 ->
+            [1];
+        _ ->
+            case floor(math:log10(N) + 1) of
+                Size when Size rem 2 == 0 ->
+                    {Left, Right} = split(N, Size),
+                    [Left, Right];
+                _ ->
+                    [N * 2024]
+            end
+    end.
 
-step([], Acc) ->
-    lists:reverse(Acc);
-step([H | T], Acc) ->
-    Acc1 =
-        case H of
-            0 ->
-                [1 | Acc];
-            N ->
-                case floor(math:log10(N) + 1) of
-                    Size when Size rem 2 == 0 ->
-                        {Left, Right} = split(N, Size),
-                        [Right, Left | Acc];
-                    _Other ->
-                        [N * 2024 | Acc]
-                end
-        end,
-    step(T, Acc1).
+all_steps_of_one(_, 0) ->
+    1;
+all_steps_of_one(N, Depth) ->
+    case ets:lookup(cache, {N, Depth}) of
+        [] ->
+            Res = lists:sum([all_steps_of_one(N1, Depth - 1) || N1 <- blink(N)]),
+            ets:insert(cache, {{N, Depth}, Res}),
+            Res;
+        [{_Key, Val}] ->
+            Val
+    end.
+
+solve(Ns, Depth) ->
+    _T = ets:new(cache, [named_table]),
+    Res = lists:sum([all_steps_of_one(N, Depth) || N <- Ns]),
+    ets:delete(cache),
+    Res.
 
 split(Bin, Size) when is_binary(Bin) ->
     Half = Size div 2,
